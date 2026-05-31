f33546a9c8fe818de048581d87ea7535e3892f57
Subhajit Das <das-rebel@users.noreply.github.com>
2026-05-31 13:11:11 +0530

    Add Apache license headers for pre-commit compliance

--- router_inference/router/a3m_router.py
+++ router_inference/router/a3m_router.py
@@ -1,3 +1,7 @@
+# Copyright 2024-present RouteWorks, Inc.
+# Licensed under the Apache License, Version 2.0
+# SPDX-License-Identifier: Apache-2.0
+
 #!/usr/bin/env python3
 from router_inference.router.base_router import BaseRouter
 import hashlib
